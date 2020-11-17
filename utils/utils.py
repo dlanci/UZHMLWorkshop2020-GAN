9a1a9431fa0a80766e0b8f4556a5e2dd63ea0c79
Davide Lancierini <Davide Lancierini davide.lancierini@cern.ch>
2020-11-17 12:02:24 +0100

    Added 1st exercise

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os 
 import pickle
+import matplotlib.pyplot as plt
 
 def load_dataset(path='/disk/lhcb_data/davide/ML_UZH_ds/', test=False):
     
@@ -28,4 +29,22 @@ def load_dataset(path='/disk/lhcb_data/davide/ML_UZH_ds/', test=False):
     
     return tuple_, tot_evts
 
+
+def generate_and_save_images(model, epoch, test_input, maxval, save_img=False):
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(16,16))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * (maxval))
+        plt.colorbar()
+        plt.axis('off')
+
+    if save_img:
+        os.makedirs('./GAN/img_outputs', exist_ok=True)
+        plt.savefig('./GAN/img_outputs/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
 
